9195501dec03ff79b495801f8252baf78d89f554
William McKee <mephisto@Williams-MacBook.local>
2017-04-18 18:02:21 -0400

    Full Game v4

--- python101.py
+++ python101.py
@@ -24,15 +24,15 @@ while True:
     not_guessed = True
     number_of_guesses = 0
     guesses_left = 5
-    print "\nI'm thinking of a number between 1-10"
+    print "\nI'm thinking of a number between 1-10\n"
 
     while not_guessed:
 
-        print "You have %d guesses to get it right\n" % (guesses_left)
+        print "Guesses remaining: %d" % (guesses_left)
 
         guess = raw_input("Pick a number: ")
         if int(guess) == random_number:
-            print "You got it!"
+            print "You got it!\n"
             not_guessed = False
     
         else:
@@ -44,12 +44,12 @@ while True:
             guesses_left -= 1
 
         if number_of_guesses >= 5:
-            print "You lose, number of guesses exceeded!"
+            print "You lose, number of guesses exceeded!\n"
             not_guessed = False
     
     again = raw_input("Play again (Y/N)?")
     if again == 'N':
-        print 'Exiting game.'
+        print 'Exiting game.\n'
         break
 
 
