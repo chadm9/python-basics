b153dda1b7f04a81a712b9c73009be86b413a5b1
William McKee <mephisto@Williams-MacBook.local>
2017-04-18 15:10:35 -0400

    python basics part 1

--- python101.py
+++ python101.py
@@ -1,1 +1,10 @@
-# print 'Hello World'
+first_name = raw_input("First Name: ")
+last_name = raw_input("Last Name: ")
+
+if first_name == last_name:
+    print "You first and last names are equal"
+
+age = raw_input("How old are you: ")
+
+if int(age) >= 21:
+    print "You can buy beer!"
