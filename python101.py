c17b7e41d1d6d8400e13a134bf07aa5a6d233253
William McKee <mephisto@Williams-MacBook.local>
2017-04-18 17:52:53 -0400

    Full Game v3

--- python101.py
+++ python101.py
@@ -23,19 +23,25 @@ while True:
     random_number = random.randint(1, 10)
     not_guessed = True
     number_of_guesses = 0
+    guesses_left = 5
+    print "\nI'm thinking of a number between 1-10"
 
     while not_guessed:
-        guess = raw_input("Guess a number from 1-10: ")
+
+        print "You have %d guesses to get it right\n" % (guesses_left)
+
+        guess = raw_input("Pick a number: ")
         if int(guess) == random_number:
             print "You got it!"
             not_guessed = False
     
         else:
             if int(guess) > random_number:
-                print guess, 'is too high!'
+                print guess, 'is too high!\n'
             else:
-                print guess, 'is too low!'
+                print guess, 'is too low!\n'
             number_of_guesses += 1
+            guesses_left -= 1
 
         if number_of_guesses >= 5:
             print "You lose, number of guesses exceeded!"
@@ -43,6 +49,7 @@ while True:
     
     again = raw_input("Play again (Y/N)?")
     if again == 'N':
+        print 'Exiting game.'
         break
 
 
