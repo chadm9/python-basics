d5925fff28b18bd9380a219a8b98f81898e48b4d
William McKee <mephisto@Williams-MacBook.local>
2017-04-20 09:00:29 -0400

    fixed de-dup

--- de-dup.py
+++ de-dup.py
@@ -1,8 +1,12 @@
 lyst = [1, 3, 5, 't', 'p', 1, 4, 'p']
+duplicate = []
+print lyst
+for i in range(len(lyst)):
+    for j in range(len(lyst)):
+        if lyst[i] == lyst[j] and i < j:
+            duplicate.append(lyst[j]) 
 
-for i in range(len(lyst)-1):
-    for j in range(len(lyst)-1):
-        if lyst[i] == lyst[j] and i != j:
-            lyst.remove(lyst.index(lyst[j]))
+for element in duplicate:
+    lyst.remove(lyst[lyst.index(element)])
 
 print lyst
